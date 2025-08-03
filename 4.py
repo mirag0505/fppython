b462212db2052e2afda644e203017a98e58a8514
alex_di <mirag0505@yandex.ru>
2025-08-03 18:57:34 +0300

    4

--- 4.py
+++ 4.py
@@ -1,11 +1,14 @@
-from pymonad.maybe import Just
+from pymonad.tools import curry
+from pymonad.maybe import Just, Nothing
 from pymonad.list import ListMonad
 
-def add(x):
-    return lambda y: x + y
+@curry(2)
+def add(x, y):
+    return x + y
 
 def add10(functor):
     return functor.map(add(10))
 
 print(add10(Just(5)))
 print(add10(ListMonad(1, 2, 3, 4)))
+print(add10(Nothing))
